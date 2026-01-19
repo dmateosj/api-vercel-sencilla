226cb6b8c36ba772915e8265a86ac3f7c518fba1
dmateosj <76000914+dmateosj@users.noreply.github.com>
2026-01-19 17:43:00 +0000

    Metodo POST añadido

--- api/futbolistas.js
+++ api/futbolistas.js
@@ -7,7 +7,26 @@ export default function handler(req, resp){
     //Configurar las cabeceras con JSON
     resp.setHeader('Content-Type', 'application/json')
 
+    //método GET
     if(req.method === "GET"){
-        return resp.status(200).json(futbolistas)
+        return resp.status(200).json(futbolistas    )
     }
+
+    //metodo POST
+    if(req.method==='POST'){
+        const nuevoJugador = req.body
+
+        if(!nuevoJugador.nombre)
+            return resp.status(400).json({error:"falta el nombre"})
+
+        const nuevo = {
+            id: Date.now(),
+            nombre: nuevoJugador.nombre
+        }
+
+        futbolistas.push(nuevo)
+        return resp.status(201).json(nuevo)
+    }
+
+    //si intentamos cualquier otro método
 }
